923ec53314603a7a6d543541eec15465619f85a5
Julien Wajsberg <felash@gmail.com>
2014-11-14 14:33:05 +0100

    working component

--- apps/sms/elements/simple-header/script.js
+++ apps/sms/elements/simple-header/script.js
@@ -4,10 +4,41 @@
 (function(exports) {
   const KNOWN_ATTRIBUTES = ['start', 'end'];
 
+  const ACTION_TYPES = {
+    menu: true,
+    back: true,
+    close: true
+  };
+
+  function mixin(base, ...prototypes) {
+    var result = Object.create(base);
+    prototypes.forEach((proto) => {
+      for (var key in proto) {
+        if (proto.hasOwnProperty(key)) {
+          result[key] = proto[key];
+        }
+      }
+    });
+
+    return result;
+  }
+
   var SimpleHeader = {
-    _start: null,
-    _end: null,
-    attributeChanged: function(name) {
+    _start: 0,
+    _end: 0,
+
+    createdCallback: function() {
+      var root = this.createShadowRoot();
+      var curDoc = document.currentScript.ownerDocument;
+      var template = curDoc.getElementById('simple-header-template');
+      // Copy the <template>
+      var clone = document.importNode(template.content, true);
+      // Append template to the Shadow Root
+      root.appendChild(clone);
+    },
+
+    attributeChangedCallback: function(name) {
+      console.log('attribute callback', name);
       if (!KNOWN_ATTRIBUTES.contains(name)) {
         return;
       }
@@ -18,6 +49,7 @@
     },
 
     attachedCallback: function() {
+      console.log('attached callback');
       KNOWN_ATTRIBUTES.forEach((name) => this._updateAttribute(name));
       this._heading = this.querySelector('h1, h2, h3, h4, h5, h6');
     },
@@ -40,12 +72,55 @@
           this.render();
         });
       }
+    },
+
+
+    /**
+     * Configure the action button based
+     * on the value of the `data-action`
+     * attribute.
+     *
+     * @private
+     */
+    configureActionButton: function() {
+      var old = this.els.actionButton.getAttribute('icon');
+      var type = this.getAttribute('action');
+      var supported = this.isSupportedAction(type);
+      this.els.actionButton.classList.remove('icon-' + old);
+      this.els.actionButton.setAttribute('icon', type);
+      this.els.inner.classList.toggle('supported-action', supported);
+      if (supported) { this.els.actionButton.classList.add('icon-' + type); }
+    },
+
+    /**
+     * Validate action against supported list.
+     *
+     * @private
+     */
+    isSupportedAction: function(action) {
+      return action && ACTION_TYPES[action];
+    },
+
+    /**
+     * Handle clicks on the action button.
+     *
+     * Fired async to allow the 'click' event
+     * to finish its event path before
+     * dispatching the 'action' event.
+     *
+     * @param  {Event} e
+     * @private
+     */
+    onActionButtonClick: function(e) {
+      var config = { detail: { type: this.getAttribute('action') } };
+      var actionEvent = new CustomEvent('action', config);
+      setTimeout(this.dispatchEvent.bind(this, actionEvent));
     }
   };
 
   exports.SimpleHeaderElement = document.registerElement(
     'simple-header', {
-      prototype: Object.create(HTMLElement.prototype, SimpleHeader)
+      prototype: mixin(HTMLDivElement.prototype, SimpleHeader)
     }
   );
 
